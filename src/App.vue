b099d061ba15947fcc7ea2392065903a51df41f6
gitRedgieGravador <iderge32@gmail.com>
2023-03-23 22:56:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar :elevation="2" app>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>Title</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-phone</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>home</h1>
+  </div>
+</template>
